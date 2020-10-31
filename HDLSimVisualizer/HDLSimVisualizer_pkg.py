2f8be2a662e960f9eb1dfca5816dab1e735cf677
hatlabcz <chz78@pitt.edu>
2020-10-31 00:35:27 -0400

    Prototype

--- HDLSimVisualizer/HDLSimVisualizer_pkg.py
+++ HDLSimVisualizer/HDLSimVisualizer_pkg.py
@@ -10,25 +10,30 @@ make it easier to generate data file for simulation input, import simulation
 output and compare it with expected block outputs.
 """
 import numpy as np
+from typing import Dict, Callable
 import matplotlib.pyplot as plt
 
 
-def int_to_slv(val: int, width: int)->str:
+def int_to_slv(val: int, width: int) -> str:
     """
         int to std_logic_vector
     """
     return np.binary_repr(val, width)
 
-def slv_to_int(s: str, width:int)->int:
+
+def slv_to_int(s: str) -> int:
     """
         std_logic_vector to int
     """
-    if s[0] == '0': # positive
-        return int(s,2)
-    else: # negative
-        return int(s,2)-(1<<width)
+    if s[0] == '0':  # positive
+        return int(s, 2)
+    else:  # negative
+        width = len(s)
+        return int(s, 2) - (1 << width)
 
-def export_binary(file_name: str, data: np.array(int), data_width: int , data_ss: int):
+
+def export_binary(file_name: str, data: np.array(int), data_width: int,
+                  supersample: int):
     """
     export data to a txt file. Each line in the txt file is a super sampled 
     std_logic_vector data.
@@ -41,7 +46,7 @@ def export_binary(file_name: str, data: np.array(int), data_width: int , data_ss
         data to be exported.
     data_width : int
         width of the std_logic_vector that the data is going to be writted as
-    data_ss : int
+    supersample : int
         data supersample.
 
     Returns
@@ -50,18 +55,21 @@ def export_binary(file_name: str, data: np.array(int), data_width: int , data_ss
 
     """
     # fill in zeros if number of data points is not multiples of data_ss
-    data = np.concatenate((data, np.zeros(len(data) % data_ss, dtype=int))) 
+    data = np.concatenate((data, np.zeros(len(data) % supersample, dtype=int)))
     with open(file_name, "w") as text_file:
-        n_lines = len(data) // data_ss
+        n_lines = len(data) // supersample
         for i in range(n_lines):
             w_line = ''
-            for j in range(data_ss):
-                w_line += int_to_slv(data[i * data_ss + j], data_width)
+            for j in range(supersample):
+                w_line = int_to_slv(data[i * supersample + j], data_width) \
+                         + w_line
             text_file.write(w_line)
-            if i != n_lines-1:
+            if i != n_lines - 1:
                 text_file.write('\n')
-                
-def import_binary(file_name: str, data_width: int , data_ss: int) -> np.array(int):
+
+
+def import_binary(file_name: str, data_width: int,
+                  supersample: int) -> np.array(int):
     """
     import data from the output of hdl simulation, change it to np.array
 
@@ -69,10 +77,9 @@ def import_binary(file_name: str, data_width: int , data_ss: int) -> np.array(in
     ----------
     file_name : str
         name of target file.
-
     data_width : int
         width of the each std_logic_vector in the output file
-    data_ss : int
+    supersample : int
         data supersample.
 
     Returns
@@ -80,8 +87,88 @@ def import_binary(file_name: str, data_width: int , data_ss: int) -> np.array(in
     data : np.array(int).
 
     """
-    data = np.zeros(10)
-    return data
+    with open(file_name, "r") as text_file:
+        data = []
+        lines = text_file.readlines()
+        for line in lines:
+            r_line = line.strip()
+            for i in range(supersample):
+                r_data = r_line[-data_width: ]
+                r_line = r_line[:-data_width ]
+                data.append(slv_to_int(r_data))
+    return np.array(data)
 
-# TODO: a class that is initialized with generaic dictionary, input data, 
-#       simulation directory, and block function
+
+class HDLSim:
+    def __init__(self, generics: Dict, data_in: np.array(int), sim_dir: str,
+                design_func: Callable, latency: int):
+        """
+
+        Parameters
+        ----------
+        generics : Dict
+            dictionary that contains the generics of the HDL block
+        data_in : np.array(int)
+            input test data
+        sim_dir : str
+            HDL simulation directory that where the input and output data
+            files are located
+        design_func : Callable
+            the function that does the job that the hdl code is expected to
+            do, for compare and verify the HDL simulation result.
+        latency : int
+            latency of the HDL IP block in cycles
+        """
+        self.generics = generics
+        self.input_width = generics["input_width"]
+        self.input_ss = generics.get("input_ss", 1)
+        self.output_width = generics["output_width"]
+        self.output_ss = generics.get("output_ss", 1)
+        self.sim_dir = sim_dir
+        self.design_func = design_func
+        # fill in zeros if number of data points is not multiples of supersample
+        self.data_in = np.concatenate(
+            (data_in, np.zeros(len(data_in) % self.input_ss, dtype=int)))
+        self.cycles = len(data_in) // self.input_ss
+        self.cyc_list = range(self.cycles)
+
+        # expected output data
+        #TODO: Latency
+        data_out_exp = design_func(self.data_in)
+        self.data_out_exp = np.roll(data_out_exp, latency)
+
+    def compareSimWithDesign(self,
+                             sim_output_file: str = "output_results.txt",
+                             plot: bool = True) -> bool:
+        """
+        compare HDL simulation data with the expected design output data.
+        Parameters
+        ----------
+        sim_output_file : str
+            file name of the output data from HDL simulation
+
+        plot : bool
+            when True, plot the comparision between simulation and design
+        Returns
+        -------
+        pass_exam : bool
+        """
+        self.data_out = import_binary(self.sim_dir + sim_output_file,
+                                      self.output_width,
+                                      self.output_ss)
+        print (self.data_out)
+        if (self.data_out == self.data_out_exp).all:
+            pass_exam = True
+        else:
+            pass_exam = False
+        if plot:
+            for i in range(self.output_ss):
+                plt.figure(i)
+                plt.plot(self.cyc_list, self.data_out_exp[i::self.output_ss],
+                         '-', label='expected output')
+                plt.plot(self.cyc_list, self.data_out[i::self.output_ss],
+                         "*", label='simulation output')
+                plt.legend()
+        return pass_exam
+
+
--- HDLSimVisualizer/test_demo_int.py
+++ HDLSimVisualizer/test_demo_int.py
@@ -7,23 +7,34 @@ Created on Tue Oct 27 20:46:24 2020
 
 import numpy as np
 import matplotlib.pyplot as plt
-import HDL_Sim_pkg as hs
+import HDLSimVisualizer_pkg as hsv
+from HDLSimVisualizer_pkg import HDLSim
 
 # ----------- generics ---------------------
-input_width = 31
-input_ss = 5 #supersample
-output_width = 35
-output_ss = 1
-
-#--------- data----------------------------
+generics = {"input_width": 31,
+            "input_ss": 5,  # supersample
+            "output_width": 35,
+            "output_ss": 1
+            }
+# --------- data----------------------------
 input_data = np.arange(0, 100, 1)
 
 
-#------------main---------------------
-hs.export_binary("input_vectors.txt", input_data, input_width, input_ss )
-def block_function(input_data):
-    
-    return 
-    
-output_data_expected = block_function(input_data)
+# --------- Design function -----------------
+def demo_int(input_data):
+    dout = [np.sum(input_data[i*5: i*5+10]) for i in range(len(input_data)//5)]
+    return np.array(dout)
+
+
+# ------------main---------------------
+sim_dir = r"C:\Users\zctid.LAPTOP-150KME16\OneDrive\Lab\FPGA\VivadoPorjects" \
+          r"\\Demodulator\Demodulator\Demodulator.sim\sim_1\behav\xsim\\"
+
+hsv.export_binary("input_vectors.txt",
+                  input_data, generics["input_width"], generics["input_ss"])
+
+latency = 2
+sim = HDLSim(generics, input_data, sim_dir, demo_int, latency)
+
+sim.compareSimWithDesign()
 
